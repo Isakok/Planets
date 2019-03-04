e73529afc5c79eb6c25208fac6f5d72b7e48dce8
Isabelle <isabelle.wkpm@gmail.com>
2019-03-04 13:06:52 +0100

    New scene with char movement with keys

---
+++ PlanetSaved/PlanetSaved/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D myRidudbody2D;
+    [SerializeField] private float movementSpeed;
+    private bool facingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        facingRight = true;
+        myRidudbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        HandleMovement(horizontal);
+
+        Flip(horizontal);
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        myRidudbody2D.velocity = new Vector2(horizontal * movementSpeed, myRidudbody2D.velocity.y);
+    }
+    
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+        }
+    }
+}
